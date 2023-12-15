2188da31bbeefc076ac80a3fd8d731f9274c5b37
MRadekTCZ <97914167+MRadekTCZ@users.noreply.github.com>
2023-12-15 15:31:56 +0100

    First step of bootloader finished
    Next steps:
    implement TCP server and client
    Convert project into linux compatible
    Slice prg into .c subfiles
    test on virtual machine
    test on raspberry pi

--- Bootloader.c
+++ Bootloader.c
@@ -18,65 +18,85 @@ int CopyDLLWithCopySuffix(LPCWSTR fileName) {
         }
 
         if (CopyFileW(fileName, copiedFileName, FALSE)) {
-            wprintf(L"File copied successfully as: %s\n", copiedFileName);
+            //wprintf(L"File copied successfully as: %s\n", copiedFileName);
 
             if (DeleteFileW(fileName)) {
-                wprintf(L"Original file deleted: %s\n", fileName);
+                //wprintf(L"Original file deleted: %s\n", fileName);
                 return 0x0;
             }
             else {
                 DWORD error = GetLastError();
-                wprintf(L"Error deleting the original file: %lu\n", error);
+                //wprintf(L"Error deleting the original file: %lu\n", error);
                 return 0xF1;
             }
         }
         else {
             DWORD error = GetLastError();
-            wprintf(L"Error copying the file: %lu\n", error);
+            //wprintf(L"Error copying the file: %lu\n", error);
             return 0xF2;
         }
     }
     else {
-        wprintf(L"The file does not exist.\n");
+        //wprintf(L"The file does not exist.\n");
         return 0xF3;
     }
 }
+int checkFilesExistence(int n) {
+    int result = 0;
 
-int OS1(int *iCycleCount)
+    for (int i = 1; i <= n; ++i) {
+        wchar_t filename[MAX_PATH];
+        swprintf(filename, MAX_PATH, L"app%d.dll", i);
+
+        FILE* file;
+        if (_wfopen_s(&file, filename, L"r") != 0) {
+            result = 0xF1; // Ustawienie wyniku na 0xF1 jeśli plik nie istnieje
+            break; // Przerwanie pętli, gdy pojawi się pierwszy nieistniejący plik
+        }
+        else {
+            fclose(file);
+        }
+    }
+
+    return result;
+}
+int checkCopiedFilesExistence(int n) {
+    int result = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        wchar_t filename[MAX_PATH];
+        swprintf(filename, MAX_PATH, L"app%d_copy.dll", i);
+
+        FILE* file;
+        if (_wfopen_s(&file, filename, L"r") != 0) {
+            result = 0xF1; // Ustawienie wyniku na 0xF1 jeśli plik nie istnieje
+            break; // Przerwanie pętli, gdy pojawi się pierwszy nieistniejący plik
+        }
+        else {
+            fclose(file);
+        }
+    }
+
+    return result;
+}
+int APP1(int *iCycleCount)
 {
-    const char* originalFileName = "OS3.exe";
-    const char* copiedFileName = "copy.exe";
-
-    if (CopyFile(originalFileName, copiedFileName, FALSE)) {
-        printf("File copied successfully.\n");
-    }
-
-        
-    system("copy.exe");
-    *iCycleCount = *iCycleCount + 1;
+    printf("Pierwsza \n");
     return *iCycleCount;
 }
-int OS2(int* iCycleCount)
+int APP2(int* iCycleCount)
 {   
-    LPCWSTR originalFileName = L"OS4.exe";
-    LPCWSTR copiedFileName = L"OS4_copy.exe";
-
-    if (CopyFileW(originalFileName, copiedFileName, FALSE)) {
-        wprintf(L"File copied successfully.\n");
-    }
-
-    system("OS4_copy.exe");
-    *iCycleCount = *iCycleCount + 1;
+    printf("Druga \n");
     return *iCycleCount;
 }
-int OS3(int* iCycleCount)
+int APP3(int* iCycleCount)
 {
     typedef void (*MyFunction)(); // Define function pointer type
     LPCWSTR dllPath = L"OS3.dll"; // Replace with the path to your DLL
 
     HMODULE hDLL = LoadLibraryW(dllPath);
     if (hDLL != NULL) {
-        wprintf(L"DLL loaded successfully.\n");
+       // wprintf(L"DLL loaded successfully.\n");
 
         // Get the address of the function from the DLL
         MyFunction myFunc = (MyFunction)GetProcAddress(hDLL, "os3");
@@ -97,7 +117,6 @@ int OS3(int* iCycleCount)
     *iCycleCount = *iCycleCount + 1;
     return *iCycleCount;
 }
-enum Bootprog { BM, APP }bootprog;
 
 int FBL(int* iCycleCount)
 {
@@ -110,20 +129,46 @@ int FBL(int* iCycleCount)
         if (result_error) break;
     }
     *iCycleCount = *iCycleCount + 1;
-    printf("Liczba wykonan programu: %d \n", *iCycleCount);
     return result_error;
 
 }
-
+typedef int (*FunctionStackPtr)(int);
 int main()
 {
     int ixCycleCount = 0;
     int iErrorCode1;
+    FunctionStackPtr actual_function_ptr[N_APPS + 1] = { FBL, APP1, APP2, APP3 };
+    printf("Boot Start \n");
+    //Beep(2000, 500);
+    iErrorCode1 = FBL(&ixCycleCount);
+    
     while (1)
     {
-        iErrorCode1 = FBL(&ixCycleCount);
-        printf("ErrorCode: %d \n", iErrorCode1);
-        Sleep(2000);
+       
+        int checkResult_copies = checkCopiedFilesExistence(N_APPS);
+        int checkResult_origin = checkFilesExistence(N_APPS);
+        //printf("Kopie: %d, Oryginaly: %d", checkResult_copies, checkResult_origin);
+        if (!checkResult_copies && checkResult_origin)
+        {
+            for (int a = 1; a <= N_APPS; a++)
+            {
+                actual_function_ptr[a](&ixCycleCount);
+            }
+            printf("APP mode \n");
+        }
+        else
+        {
+            //iErrorCode1 = FBL(&ixCycleCount);
+            iErrorCode1 = actual_function_ptr[0](&ixCycleCount);
+            printf("FBL mode \n");
+        }
+
+        
+
+
+        printf("Liczba wykonan programu: %d \n", ixCycleCount);
+        
+        Sleep(5000);
     }
 
     return 0;
